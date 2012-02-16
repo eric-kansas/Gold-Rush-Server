b79efcb3b06e336adce79b290a93d923b93cd53c
Ralph Bean <rbean@redhat.com>
2012-02-16 15:26:26 -0500

    Added basic bootstrap.

--- wsgi/tg2app/tg2app/websetup/bootstrap.py
+++ wsgi/tg2app/tg2app/websetup/bootstrap.py
@@ -14,38 +14,28 @@ def bootstrap(command, conf, vars):
     # <websetup.bootstrap.before.auth
     from sqlalchemy.exc import IntegrityError
     try:
-        u = model.User()
-        u.user_name = u'manager'
-        u.display_name = u'Example manager'
-        u.email_address = u'manager@somedomain.com'
-        u.password = u'managepass'
-    
-        model.DBSession.add(u)
-    
-        g = model.Group()
-        g.group_name = u'managers'
-        g.display_name = u'Managers Group'
-    
-        g.users.append(u)
-    
-        model.DBSession.add(g)
-    
-        p = model.Permission()
-        p.permission_name = u'manage'
-        p.description = u'This permission give an administrative right to the bearer'
-        p.groups.append(g)
-    
-        model.DBSession.add(p)
-    
-        u1 = model.User()
-        u1.user_name = u'editor'
-        u1.display_name = u'Example editor'
-        u1.email_address = u'editor@somedomain.com'
-        u1.password = u'editpass'
-    
-        model.DBSession.add(u1)
-        model.DBSession.flush()
+        # Add three lulzy users.
+        player1 = model.Player(
+            name="Lulzy Guy"
+        )
+        model.DBSession.add(player1)
+        player2 = model.Player(
+            name="Lulzy Lady"
+        )
+        model.DBSession.add(player2)
+
+        player3 = model.Player(
+            name="Foo Manchu"
+        )
+        model.DBSession.add(player3)
+
+        player1.friends.append(player2)
+        player2.friends.append(player1)
+        player1.friends.append(player3)
+        player3.friends.append(player1)
+
         transaction.commit()
+
     except IntegrityError:
         print 'Warning, there was a problem adding your auth data, it may have already been added:'
         import traceback
